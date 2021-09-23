5fef016dbc511da7708f24985151ea1cc04b3530
Agsut <70062634+Agsut@users.noreply.github.com>
2021-09-23 14:17:38 +0200

    Add files via upload

---
+++ webbshop.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 23 sep 2021 kl 14:10
+-- Serverversion: 10.4.20-MariaDB
+-- PHP-version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `webbshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `customers`
+--
+
+CREATE TABLE `customers` (
+  `customerid` int(10) UNSIGNED NOT NULL,
+  `username` varchar(20) COLLATE utf8_swedish_ci NOT NULL,
+  `firstname` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+  `lastname` varchar(100) COLLATE utf8_swedish_ci NOT NULL,
+  `address` varchar(100) COLLATE utf8_swedish_ci NOT NULL,
+  `zip` int(10) NOT NULL,
+  `city` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+  `phone` varchar(15) COLLATE utf8_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Dumpning av Data i tabell `customers`
+--
+
+INSERT INTO `customers` (`customerid`, `username`, `firstname`, `lastname`, `address`, `zip`, `city`, `phone`) VALUES
+(1, 'kalle', 'Kalle', 'Anka', 'Stora gatan 1', 12345, 'Ankeborg', '123346');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderid` int(10) UNSIGNED NOT NULL,
+  `produktid` int(10) UNSIGNED NOT NULL,
+  `antal` int(5) UNSIGNED NOT NULL,
+  `customerid` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Dumpning av Data i tabell `orders`
+--
+
+INSERT INTO `orders` (`orderid`, `produktid`, `antal`, `customerid`) VALUES
+(1, 1, 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `products`
+--
+
+CREATE TABLE `products` (
+  `products` int(10) UNSIGNED NOT NULL,
+  `name` varchar(20) COLLATE utf8_swedish_ci NOT NULL,
+  `description` varchar(200) COLLATE utf8_swedish_ci NOT NULL,
+  `price` int(50) NOT NULL,
+  `picture` varchar(200) COLLATE utf8_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Dumpning av Data i tabell `products`
+--
+
+INSERT INTO `products` (`products`, `name`, `description`, `price`, `picture`) VALUES
+(1, 'Päron', 'Frukt', 100, '');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(20) COLLATE utf8_swedish_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8_swedish_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `status` tinyint(2) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Dumpning av Data i tabell `users`
+--
+
+INSERT INTO `users` (`username`, `email`, `password`, `status`) VALUES
+('kalle', 'kalle@exempel.se', 'qwe123', 1);
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customerid`),
+  ADD KEY `username` (`username`);
+
+--
+-- Index för tabell `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderid`),
+  ADD KEY `produktid` (`produktid`),
+  ADD KEY `customerid` (`customerid`);
+
+--
+-- Index för tabell `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`products`);
+
+--
+-- Index för tabell `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `password` (`password`);
+
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customerid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT för tabell `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT för tabell `products`
+--
+ALTER TABLE `products`
+  MODIFY `products` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restriktioner för dumpade tabeller
+--
+
+--
+-- Restriktioner för tabell `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customerid`) REFERENCES `customers` (`customerid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`produktid`) REFERENCES `products` (`products`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restriktioner för tabell `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`username`) REFERENCES `customers` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
