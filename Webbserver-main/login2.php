0753afa075ca0606d087af440406e81a9dc87caf
Agsut <70062634+Agsut@users.noreply.github.com>
2021-12-15 14:03:28 +0100

    Add files via upload

---
+++ Webbserver-main/login2.php
@@ -0,0 +1,34 @@
+<?php 
+    if(empty($_POST['username'])) empty($_POST['password'])){
+        header('Location:login.php');
+    }
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+
+    require "../includes/connect.php";
+
+    $sql = "SELECT password,status FROM users WHERE username=?";
+    $res = $dbh->prepare($sql);
+    $res->bind_param("s",$username);
+    $res->execute();
+    $res = $res->get_result();
+    $row = $res->fetch_assoc();
+
+    if(!row){
+        //användaren finns inte
+        echo "felaktig användare";
+    }
+    else{
+        //användaren finns
+        if($row['password']===$password){
+            session_start();
+            $_SESSION['username'] = $username;
+            $_SESSION['status'] = $row['status'];
+            echo "Inloggad";
+
+        }
+        else{
+            //felaktigt lösenord
+            echo "Felaktigt lösenord";
+        }
+    }
