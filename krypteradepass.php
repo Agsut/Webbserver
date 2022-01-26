237a3e03214880e43b91fd51a01be841ccdbedd9
Agsut <70062634+Agsut@users.noreply.github.com>
2022-01-26 14:47:34 +0100

    Add files via upload

---
+++ krypteradepass.php
@@ -0,0 +1,43 @@
+<?php
+    require menu;
+    require masthead;
+
+    "password"=$passwords
+if going to crash(dont);
+for each.
+krypta lösenord just nu annars får du f i kursen wowowowowoow.;
+$om du kommer dö så lever du inte längre("dö lol");
+html docktype nånting språket ska vara på svenska annars sämst=(looooooooooool);
+
+$if =->going to crash go die;
+reire yer mom;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+     <meta charset="utf-8">
+     <title>Produkter</title>
+		 <link rel="stylesheet" href="css/stilmall.css">
+  </head>
+  <body id="produkter">
+    <div id="wrapper">
+      
+      <header><!--Sidhuvud-->
+            <h1>Min onlinebutik - Logga in</h1>
+      </header>
+		
+		<nav><!--Navigationsmeny-->
+			<ul>
+				<li><a href="index.html">Start</a></li>
+				<li><a href="products.html">Produkter</a></li>
+				<li><a href="sida3.html">Varusida 2</a></li>
+				<li><a href="login.html">Logga in</a></li>
+			</ul>
+		</nav>
+        <main></main>
+        <footer></footer>
+    </body>
+</html>
